8076f9a361ea9fc761f8dbd17baa01326ae639c9
Cherise Foster <cherise.foster@outlook.com>
2024-02-06 14:03:46 -0700

    started navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar(){
+
+    return (
+        <nav className='navbar'>
+            <a href='https://www.connect13.co/' className='logo'>Connect<strong>13</strong>
+                <img src="//www.connect13.co/wp-content/uploads/2020/02/connect_logo_W_sm.png" alt='Connect13'/>
+            </a>
+        </nav>
+    )
+}
+
+export default NavBar;
