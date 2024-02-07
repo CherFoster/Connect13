0250bd649b792ded30e282802a4bafd3211feb2e
Cherise <cherise.foster@outlook.com>
2024-02-06 19:28:23 -0700

    added h1 to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,13 @@ import '../style/Header.css';
 function Header() {
     
     return (
-        <div className="image container">
-            <img className='banner' src='https://i.postimg.cc/15RjRxX0/Making-Impactful-Connections.png' alt="banner" />
+        <div className="banner-container">
+            <div className="image-container">
+                <img className='banner' src='https://i.postimg.cc/05fLwTWQ/Making-Impactful-Connections-1.png' alt="banner" />
+            </div>
+            <div className="text-content">
+                <h1>Making Impactful Connections</h1>
+            </div>
         </div>
     )
 }
