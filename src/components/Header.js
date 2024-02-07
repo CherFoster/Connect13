b65b027d6c5e4be33dc8692d24ad8e599462916c
Cherise <cherise.foster@outlook.com>
2024-02-06 17:24:48 -0700

    changed banner picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import '../style/Header.css';
 function Header() {
     
     return (
-        <div>
-            <img className='banner' src='https://i.postimg.cc/Yq8W6JrC/Banner.png' alt="banner" />
+        <div className="image container">
+            <img className='banner' src='https://i.postimg.cc/15RjRxX0/Making-Impactful-Connections.png' alt="banner" />
         </div>
     )
 }
