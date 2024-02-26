976e7422b4ddb06cdc37fe42b000dca2f5cedb12
Cherise Foster <cherise.foster@outlook.com>
2024-02-26 13:42:02 -0700

    updated banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <div className="banner-container">
             <div className="image-container">
-                <img className='banner' src='https://i.postimg.cc/05fLwTWQ/Making-Impactful-Connections-1.png' alt="banner" />
+                <img className='banner' src='https://i.postimg.cc/KcwRVgsQ/Making-Impactful-Connections-1.png' srcset="medium.jpg 1000w large.jpg 2000w" alt="banner" />
             </div>
             <div className="text-content">
                 <h1>Making Impactful Connections</h1>
