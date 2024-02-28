1ef47fe047a01285d5bff97cd83dd2b2326fb63e
Cherise Foster <cherise.foster@outlook.com>
2024-02-27 17:07:41 -0700

    updated scss

--- src/components/HomeCards.js
+++ src/components/HomeCards.js
@@ -1,13 +1,12 @@
 import React from "react";
 import '../style/HomeCards.scss'; 
 
-function HomeCards({image, title, text, copy }){
+function HomeCards({ title, text, copy }){
     
     return (
         <div className="card">
-          <div className="image" style={{ backgroundImage: `url(${image})` }}></div>
           <div className="content">
-            <h3 className="title">{title}</h3>
+            <h2 className="title">{title}</h2>
             <p className="text">{text}</p>
             <p className="copy">{copy}</p>
           </div>
